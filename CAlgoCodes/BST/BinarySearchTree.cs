0d43a65d8ff4babfe3ccbb2934348210c009fa45
Asad Gul <asadgul015@gmail.com>
2024-03-24 14:48:41 +0500

    BST implemented

---
+++ CAlgoCodes/BST/BinarySearchTree.cs
@@ -0,0 +1,104 @@
+﻿using CAlgoCodes.DataStructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAlgoCodes.BST
+{
+    public class BinarySearchTree
+    {
+        public Nodes CreateBst(Nodes root,int value)
+        {
+            if (root == null)
+            {
+                root=new Nodes(value);
+                return root;
+            }
+            if (root.data > value)
+            {
+             root.Left= CreateBst(root.Left, value);
+            }
+            else
+            {
+                root.Right= CreateBst(root.Right, value);
+            }
+
+            return root;
+        }
+        public void InOrderTraversal(Nodes root)
+        {
+            if (root == null)
+            {
+                return ;
+            }
+            InOrderTraversal(root.Left);
+            Console.WriteLine(root.data);
+            InOrderTraversal(root.Right);
+        }
+        public Boolean SearchNode(Nodes root,int key)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+            if (root.data > key)
+            {
+                return SearchNode(root.Left, key);
+            }
+            else if (root.data == key)
+            {
+                return true;
+            }
+            else
+            {
+                return SearchNode(root.Right, key);
+            }
+        }
+        public Nodes DeleteNode(Nodes root,int value)
+        {
+            if (root.data > value)
+            {
+                root.Left=DeleteNode(root.Left, value);
+            }
+            else if (root.data < value)
+            {
+                root.Right = DeleteNode(root.Right, value);
+            }
+            else
+            {
+                if(root.Left==null && root.Right == null)
+                {
+                    return null;
+                }
+                else if (root.Left==null)
+                {
+                    return root.Right;
+                }
+                else if (root.Right == null)
+                {
+                    return root.Left;
+                }
+                else
+                {
+                    Nodes rootd = IsSuccessor(root.Right);
+                    root.data = rootd.data;
+                    root.Right=DeleteNode(root.Right, rootd.data);
+                }
+
+            }
+            return root;
+
+        }
+        public Nodes IsSuccessor(Nodes nodes)
+        {
+            while (nodes.Left != null)
+            {
+                nodes = nodes.Left;
+            }
+            return nodes;
+        }
+
+    }
+}
--- CAlgoCodes/Program.cs
+++ CAlgoCodes/Program.cs
@@ -1,21 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CAlgoCodes.Binary_Trees;
+using CAlgoCodes.BST;
 using CAlgoCodes.DataStructure;
 using CAlgoCodes.DataStructure.Queues;
 using CAlgoCodes.DataStructure.StackData_Structure;
-//Binary Tree
-BinaryTree binaryTree = new BinaryTree();
-//Nodes nodes = binaryTree.CreateTree(new int[] { 1, 2, 3, -1, -1, 5, 6, 7, 9, -1, -1, 10 });
-Nodes nodes = binaryTree.CreateTree(new int[] { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1,6, -1,-1 });
-Console.WriteLine("Preorder");
-binaryTree.Preorder(nodes);
+//BST
 
-Console.WriteLine("Inorder");
-binaryTree.Inorder(nodes);
-Console.WriteLine("Postorder");
-binaryTree.Postorder(nodes);
-Console.WriteLine("Total Nodes");
-Console.WriteLine( binaryTree.CountofNode(nodes));
+BinarySearchTree binarySearchTree = new BinarySearchTree();
+Nodes root = null;
+int[] val = { 5, 1, 4, 2, 3, 7 };
+for(int i = 0; i < val.Length; i++)
+{
+  root= binarySearchTree.CreateBst(root, val[i]);
+}
+binarySearchTree.InOrderTraversal(root);
+if (binarySearchTree.SearchNode(root, 3))
+{
+    Console.WriteLine("Key Found");
+}
+else
+{
+    Console.WriteLine("Key Not Found");
+}
+binarySearchTree.DeleteNode(root, 1);
+Console.WriteLine("After Deletion");
+binarySearchTree.InOrderTraversal(root);
+
+Console.ReadLine();
+//Binary Tree
+//BinaryTree binaryTree = new BinaryTree();
+////Nodes nodes = binaryTree.CreateTree(new int[] { 1, 2, 3, -1, -1, 5, 6, 7, 9, -1, -1, 10 });
+//Nodes nodes = binaryTree.CreateTree(new int[] { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1,6, -1,-1 });
+//Console.WriteLine("Preorder");
+//binaryTree.Preorder(nodes);
+
+//Console.WriteLine("Inorder");
+//binaryTree.Inorder(nodes);
+//Console.WriteLine("Postorder");
+//binaryTree.Postorder(nodes);
+//Console.WriteLine("Total Nodes");
+//Console.WriteLine( binaryTree.CountofNode(nodes));
 
 
 //Console.WriteLine(nodes.data);
@@ -59,15 +83,15 @@ Console.WriteLine( binaryTree.CountofNode(nodes));
 //}
 
 //Queue Linked List
-QueueLinkedList queueLinkedList = new QueueLinkedList();
-queueLinkedList.Enqueue(1);
-queueLinkedList.Enqueue(2);
-queueLinkedList.Enqueue(3);
-queueLinkedList.Enqueue(4);
+//QueueLinkedList queueLinkedList = new QueueLinkedList();
+//queueLinkedList.Enqueue(1);
+//queueLinkedList.Enqueue(2);
+//queueLinkedList.Enqueue(3);
+//queueLinkedList.Enqueue(4);
 
-while (!queueLinkedList.IsEmpty())
-{
-    Console.WriteLine(queueLinkedList.Peek());
-    queueLinkedList.Remove();
-}
-Console.ReadLine();
+//while (!queueLinkedList.IsEmpty())
+//{
+//    Console.WriteLine(queueLinkedList.Peek());
+//    queueLinkedList.Remove();
+//}
+//Console.ReadLine();
